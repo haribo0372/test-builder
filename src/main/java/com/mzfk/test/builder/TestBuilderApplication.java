d9c572246c69fe27e50fb786778632c979eb0625
haribo0372 <sasha530000@gmail.com>
2024-11-20 20:19:56 +0300

    refactor by h2 db

--- src/main/java/com/mzfk/test/builder/TestBuilderApplication.java
+++ src/main/java/com/mzfk/test/builder/TestBuilderApplication.java
@@ -19,8 +19,5 @@ public class TestBuilderApplication {
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		System.setProperty("TOKEN_KEY", dotenv.get("TOKEN_KEY"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
 	}
 }
